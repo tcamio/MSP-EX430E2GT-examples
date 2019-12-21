ac2f104732d0096be228922aa34d9ff3604783eb
Toshiki Kamio <41144380+tcamio@users.noreply.github.com>
2019-12-21 14:34:24 -0700

    Blink red LED

--- 01.Basics/Blink/Blink.ino
+++ 01.Basics/Blink/Blink.ino
@@ -1,1 +1,17 @@
+/*
+ * Blink
+ * Turns on red LED on for one second, then off for one second, repeatedly.
+ */
 
+const int red_led = 14;
+
+void setup() {
+  pinMode(red_led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red_led, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(1000);                 // wait for a second
+  digitalWrite(red_led, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                 // wait for a second
+}
